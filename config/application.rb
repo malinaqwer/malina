6d6794488bfa7332b3968291d70a69a8bb6d387b
iarte <yandeexart@yandex.ru>
2014-02-16 00:01:52 +0600

    mongo

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails/all" 
+require "rails/all"
+require "sprockets/railtie" 
 # require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "active_resource/railtie"
@@ -23,5 +24,6 @@ module Mongo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.enabled = false
   end
 end

22b600122ea18d766f2317be1649f0f923f6cd4a
iarte <yandeexart@yandex.ru>
2014-02-18 06:39:36 +0600

    pages

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -10,8 +10,8 @@ class AreasController < ApplicationController
   
   def sitemap
     i = params[:page].to_i
-    min = (i - 1)*90 + 1
-    max = i*90
+    min = (i - 1)*80 + 1
+    max = i*80
     @pages = Page.all
     @areas = Area.all[min..max]
   end

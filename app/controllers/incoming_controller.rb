ed683b8fd91e33e17115634852dcca2073d8c5ba
iarte <yandeexart@yandex.ru>
2014-03-06 22:44:05 +0600

    commit

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,4 +1,6 @@
 class IncomingController < ApplicationController
+  require 'mail'
+  skip_before_filter :verify_authenticity_token
   def event
     Rails.logger.log Logger::INFO, params
     render text: 'success', status: 200 # a status of 404 would reject the mail

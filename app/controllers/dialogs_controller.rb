1cd1cc7ebdf4d5de63a997f70a5cf320208bd46d
iarte <yandeexart@yandex.ru>
2014-03-16 13:37:09 +0600

    vdfvd

--- app/controllers/dialogs_controller.rb
+++ app/controllers/dialogs_controller.rb
@@ -37,7 +37,7 @@ class DialogsController < ApplicationController
 
 
   def enter
-    unless request.remote_ip.include?('66.249.66') || request.remote_ip.include?('66.249.76')
+    unless request.remote_ip.include?('66.249.66') || request.remote_ip.include?('66.249.76') || request.remote_ip.include?('66.249.64')
       if params[:on].present?
         @dialog = Dialog.where(id: params[:on]).first || Dialog.create(ip: request.remote_ip, url_start: params[:url])
       else
